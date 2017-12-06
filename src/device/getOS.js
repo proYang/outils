6e8e82cffe608c442b5bef81b015ba6918a42eae
SlaneYang <904509221@qq.com>
2017-12-06 18:27:06 +0800

    :bug:修复getOS缺少return的问题

--- src/device/getOS.js
+++ src/device/getOS.js
@@ -11,7 +11,7 @@ function getOS() {
     if (/mac/i.test(appVersion)) return 'MacOSX'
     if (/win/i.test(appVersion)) return 'windows'
     if (/linux/i.test(appVersion)) return 'linux'
-    if (/iphone/i.test(userAgent) || /ipad/i.test(userAgent) || /ipod/i.test(userAgent)) 'ios'
+    if (/iphone/i.test(userAgent) || /ipad/i.test(userAgent) || /ipod/i.test(userAgent)) return 'ios'
     if (/android/i.test(userAgent)) return 'android'
     if (/win/i.test(appVersion) && /phone/i.test(userAgent)) return 'windowsPhone'
 }
