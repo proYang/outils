58f08d4ebf631dfd28dd7fd236b098a194a6941d
聪明的竹子 <yang759126596@163.com>
2018-03-23 14:51:11 +0800

    update isPhoneNum reg

--- src/regexp/isPhoneNum.js
+++ src/regexp/isPhoneNum.js
@@ -5,7 +5,7 @@
  * @return {Boolean} 
  */
 function isPhoneNum(str) {
-    return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(str)
+    return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57]|19[0-9])[0-9]{8}$/.test(str)
 }
 
 module.exports = isPhoneNum
